61fce39d72753daaa91cbfa37d9b13d6a999edf1
EmreIscan-47 <iscan.connect@outlook.de>
2025-07-17 14:21:38 +0200

    Ring of Fire Angular Project start

---
+++ ringoffire/src/app/game/game.ts
@@ -0,0 +1,15 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-game',
+  imports: [CommonModule],
+  templateUrl: './game.html',
+  styleUrl: './game.scss'
+})
+export class Game {
+  pickCardAnimation = false;
+takeCard() {
+  this.pickCardAnimation = true;
+}
+}
